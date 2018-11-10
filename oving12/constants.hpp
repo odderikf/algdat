6553a943b5e60b684dd9b960ac93e5dbcf391cc4
odderikf <odderikf@stud.ntnu.no>
2018-11-10 01:49:12 +0100

    added constants file, references to it currently only in bitset branch

---
+++ oving12/constants.hpp
@@ -0,0 +1,20 @@
+//
+// Created by odderikf on 11/10/18.
+//
+
+#ifndef OVING12_CONSTANTS_HPP
+#define OVING12_CONSTANTS_HPP
+
+#include <boost/filesystem.hpp>
+
+const int BYTE_SIZE = 256;
+const int BLOCK_SIZE = 8192;
+const int COUNTS_SIZE = 2048;
+
+const boost::filesystem::path test_in("test_in");
+const boost::filesystem::path test_out("test_out");
+const boost::filesystem::path test_huff("test_huff");
+const boost::filesystem::path huff("huff");
+const boost::filesystem::path dot(".");
+
+#endif //OVING12_CONSTANTS_HPP
